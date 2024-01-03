402866e82953e3bc45a6855ea7da0e6ace3bcdb0
微光北下 <wgbx@foxmail.com>
2024-01-03 19:48:45 +0800

    feat: createApp基础实现

--- packages/core/index.js
+++ packages/core/index.js
@@ -3,23 +3,16 @@ export function createApp(options) {
   return {
     mount(selector) {
       const parent = document.querySelector(selector)
-      console.log('🚀 ~ file: index.js:5 ~ mount ~ parent:', parent)
       if (!options.render) {
-        // 如果没有配置渲染函数，使用自定义的编译函数得到渲染函数
         options.render = this.compile(parent.innerHTML)
       }
-      // 通过call执行函数，并将setup的返回值作为this
       const el = options.render.call(options.setup())
-      // 清空宿主元素的内容
       parent.innerHTML = ''
-      // 将el追加到宿主元素
       parent.appendChild(el)
     },
     compile(template) {
-      // template暂时不处理
       return function render() {
         const div = document.createElement('div')
-        // 这里的this就是setup函数的返回值
         div.innerHTML = this.message.value
         return div
       }
