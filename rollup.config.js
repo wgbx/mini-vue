a2b384c120b056c4357fb3477fc498fb43854c33
微光北下 <wgbx@foxmail.com>
2024-01-04 20:02:26 +0800

    feat: 添加build文件

--- rollup.config.js
+++ rollup.config.js
@@ -3,29 +3,38 @@ import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
 
-const name = 'tool'
-
-export default {
-  input: './packages/core/index.ts',
-  output: [
-    {
-      name,
-      file: `dist/${name}.js`,
-      format: 'esm',
-      plugins: [terser()]
-    }
-  ],
-  plugins: [
-    NodeResolve(),
-    commonjs(),
-    typescript({
-      outDir: 'dist/'
-    }),
-    babel({
-      extensions: ['js', 'ts'],
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**'
-    })
-  ]
+const require = createRequire(import.meta.url)
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const packagesDir = path.resolve(__dirname, 'packages')
+const packageDir = path.resolve(packagesDir, process.env.TARGET)
+console.log('🚀 ~ file: rollup.config.js:14 ~ packageDir:', packageDir)
+const name = path.basename(packageDir)
+const resolve = p => path.resolve(packageDir, p)
+const pkg = require(resolve('package.json'))
+const { buildOptions = {} } = pkg
+console.log('🚀 ~ file: rollup.config.js:18 ~ name:', name)
+console.log('🚀 ~ file: rollup.config.js:17 ~ buildOptions:', buildOptions)
+const outputOptions = {
+  es: {
+    file: resolve(`dist/${name}.esm.js`),
+    format: 'es'
+  },
+  cjs: {
+    file: resolve(`dist/${name}.cjs.js`),
+    format: 'cjs'
+  },
+  global: {
+    file: resolve(`dist/${name}.global.js`),
+    format: 'iife'
+  }
 }
+
+function createConfig(){
+
+}
+
+export default buildOptions.formats.map(format => createConfig(format, outputOptions[format]))
--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ import { execa } from 'execa'
 const dirs = fs.readdirSync('packages').filter(path => fs.statSync(`packages/${path}`).isDirectory())
 
 async function build(target) {
-  await execa('rollup', ['-c', '--environment', `TARGET:${target}`])
+  await execa('rollup', ['-c', '--environment', `TARGET:${target}`], { stdio: 'inherit' })
 }
 
 Promise.all(dirs.map(build)).then(() => {
