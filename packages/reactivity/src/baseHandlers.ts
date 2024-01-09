0dea9860c34949c3774087fcab62bf3bba082996
微光北下 <wgbx@foxmail.com>
2024-01-09 21:21:36 +0800

    feat: 添加setter钩子

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,23 +1,53 @@
+import { isObject } from '@vue/shared'
+import { reactive, readonly } from './reactive'
+
 const get = createGetter()
 const shallowGet = createGetter(false, true)
 const readonlyGet = createGetter(true)
 const shallowReadonlyGet = createGetter(true, true)
 
+const set = createSetter()
+const shallowSet = createSetter(true)
+
 function createGetter(isReadonly = false, shallow = false) {
   return function get(target, key, receiver) {
-    
+    const res = Reflect.get(target, key, receiver)
+    if (!isReadonly) {
+    }
+    if (shallow) {
+      return res
+    }
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
+    return res
+  }
+}
+
+function createSetter(shallow = false) {
+  return function set(target, key, value, receiver) {
+    const res = Reflect.set(target, key, value, receiver)
+    return res
   }
 }
 
 export const reactiveHandlers = {
-  get
+  get,
+  set
 }
 export const shallowReactiveHandlers = {
-  get: shallowGet
+  get: shallowGet,
+  set: shallowSet
 }
 export const readonlyHandlers = {
-  get: readonlyGet
+  get: readonlyGet,
+  set: () => {
+    console.info('this key is readonly, set failed')
+  }
 }
 export const shallowReadonlyHandlers = {
-  get: shallowReadonlyGet
+  get: shallowReadonlyGet,
+  set: (target, key, value) => {
+    console.info('this key is readonly, set failed')
+  }
 }
