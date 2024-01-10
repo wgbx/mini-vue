1c4138382801ecf8fe9060512b1ff456ef9eda0d
微光北下 <wgbx@foxmail.com>
2024-01-10 21:46:43 +0800

    feat: 添加Track

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,5 +1,6 @@
 let uid = 0
 let activeEffect
+let targetMap = new WeakMap()
 const effectStack = []
 
 export function effect(fn, options: any = {}) {
@@ -12,13 +13,15 @@ export function effect(fn, options: any = {}) {
 
 function createReactiveEffect(fn, options) {
   const effect = function reactiveEffect() {
-    try {
-      effectStack.push(effect)
-      activeEffect = effect
-      fn()
-    } finally {
-      effectStack.pop()
-      activeEffect = effectStack[effectStack.length - 1]
+    if (!effectStack.includes(effect)) {
+      try {
+        effectStack.push(effect)
+        activeEffect = effect
+        fn()
+      } finally {
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+      }
     }
   }
   effect.id = uid++
@@ -29,5 +32,18 @@ function createReactiveEffect(fn, options) {
 }
 
 export function Track(target, type, key) {
-  console.log(target, type, key, activeEffect)
+  if (activeEffect === undefined) {
+    return
+  }
+  let depMap = targetMap.get(target)
+  if (!depMap) {
+    targetMap.set(target, (depMap = new Map()))
+  }
+  let dep = depMap.get(key)
+  if (!dep) {
+    depMap.set(key, (dep = new Set()))
+  }
+  if(!dep.has(activeEffect)){
+    dep.add(activeEffect)
+  }
 }
